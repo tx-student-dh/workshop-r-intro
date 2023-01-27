6839166d2e1186fed3bcfce8948b63b7372c3ced
Amy Shreeve <79295223+amyeshreeve@users.noreply.github.com>
2023-01-26 20:41:29 -0600

    init commit

---
+++ RIntroduction.R
@@ -0,0 +1,14 @@
+#What is the difference between the console and the script editor?
+
+# What do the + and > mean in the console?
+
+# How do I assign variable names?
+
+x <- 3 + 8
+y <- "A quick red fox jumps over the lazy dog."
+
+# How do I open packages?
+
+library(dplyr)
+
+#How do I start a new script?
